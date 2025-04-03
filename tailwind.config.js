aae1484acadffac3c183f6066da5c41bf315be52
haidarniv <711haidarhanif@gmail.com>
2025-04-03 21:32:29 +0700

    first commit eduwork sinergi

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'instru': ['"Instrument Sans"', 'sans-serif'],
+        'bebas': ['"Bebas Neue"']
+      },
+      colors: {
+
+      }
+    },
+  },
+  plugins: [],
+}
+
